efa97bf6ef82d89bcc4ff9a484843a26cf1502a1
galagaking <frank@beksonline.nl>
2018-06-10 21:32:41 +0200

    Update Payload.js

--- Payload.js
+++ Payload.js
@@ -5,8 +5,8 @@ var humidity = 5+3*((bytes[3] >> 3) & 0x1F);
 var co2 = 4*(((bytes[3] & 0x07) << 8) | bytes[2]);
 var tvoc = 5*bytes[4];
 return {
-mbar: mbar,
-celcius: temperature / 100.0,
+pressure: mbar,
+temperature: temperature / 100.0,
 humidity:humidity,
 co2:co2,
 tvoc:tvoc
